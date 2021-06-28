39efde1fdacbc0cd83d396d807b7fe7b1603f890
Francisco Ciccone <franciscociccone4@gmail.com>
2021-06-28 14:10:15 +0200

    Added products on HTML through JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const productos = [
+const products = [
     {id:1, title:'gascoigne', price:500},
     {id:2, title:'maradona', price:500},
-    {id:3, title:'matthaus', price:500},
+    {id:3, title:'matthäus', price:500},
     {id:4, title:'schmeichel', price:500},
     {id:5, title:'valderrama', price:500},
     {id:6, title:'milla', price:500},
     {id:7, title:'romario', price:500},
     {id:8, title:'baggio', price:500},
     {id:9, title:'voller', price:500},
-    {id:10, title:'sanchez', price:500},
+    {id:10, title:'sánchez', price:500},
     {id:11, title:'batistuta', price:500},
     {id:12, title:'stoichkov', price:500},
     {id:13, title:'weah', price:500},
@@ -21,4 +21,23 @@ const productos = [
     {id:20, title:'van basten', price:500},
     {id:21, title:'hagi', price:500},
     {id:22, title:'karpin', price:500},
+    {id:23, title:'laudrup', price:500},
+    {id:24, title:'litmanen', price:500},
 ]
+
+const productsContainer = document.querySelector('section.products')
+console.log(productsContainer)
+
+let list = '';
+
+products.forEach(products => {
+    list += `
+    <article class='products products-${products.id}'>
+            <h4>${products.title}</h4>
+            <p>${products.price}</p>
+            <button>Buy</button>
+    </article>
+    `;
+})
+
+productsContainer.innerHTML = list;
